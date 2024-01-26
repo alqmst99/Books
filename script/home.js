814cbb2f9e59dc9e923abe7fe9b87123da2dc096
Nahuel Pierini <Nahuel.Pierini90@hotmail.com>
2024-01-26 18:44:49 -0300

    Home maquetacion, styles, Swiper11

---
+++ script/home.js
@@ -0,0 +1,23 @@
+var swiper = new Swiper(".bookSlide", {
+    loop:true,
+    centeredSlides:true,
+    autoplay:{
+       delay: 9500,
+       disableOnInteraction:false,
+    },
+       breakpoints: {
+         0: {
+           slidesPerView: 1,
+       
+         },
+         768: {
+           slidesPerView: 2,
+         
+         },
+         1024: {
+           slidesPerView: 3,
+         
+         },
+      
+       },
+     });
---
+++ script/script.js
@@ -0,0 +1,48 @@
+
+/*############## Header ##############*/
+
+//view responsive search form
+const searchForm = document.querySelector('.search-form');
+const searchBtn= document.querySelector('#search-btn');
+searchBtn.addEventListener('click', ()=>{
+    searchForm.classList.toggle('on');
+});
+
+//Fixed Header
+window.onscroll= ()=>{
+    searchForm.classList.remove('on');
+    if(window.scrollY > 80){
+
+        document.querySelector('.header-2').classList.add('active');
+    }
+    else{
+
+        document.querySelector('.header-2').classList.remove('active');
+    }
+};
+//Login Form
+
+let loginForm= document.querySelector('.login-form-container')
+let loginIco= document.querySelector('.login')
+let loginClose= document.querySelector('#close-login')
+
+loginIco.addEventListener('click', ()=>{
+    loginForm.classList.add('view');
+})
+loginClose.addEventListener('click', ()=>{
+    loginForm.classList.remove('view');
+})
+//Recharge 
+window.onload= ()=>{
+    if(window.scrollY > 80){
+
+        document.querySelector('.header-2').classList.add('active');
+    }
+    else{
+
+        document.querySelector('.header-2').classList.remove('active');
+    }
+};
+
+//slider
+
